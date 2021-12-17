c336e1d61cf2bbdddd9d3a4765876618d27cd308
jnbeach <joseph.n.beach@outlook.com>
2021-12-17 14:57:00 -0600

    Updated SplitString

---
+++ SplitString/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine($"Input a string to pairify!");
+
+string input = Console.ReadLine();
+string[] result = Solution(input);
+foreach (string str in result)
+{
+    Console.WriteLine(str);
+}
+
+
+static string[] Solution(string str)
+{
+    if (str.Length % 2 != 0)
+    {
+        str += "_";
+    }
+
+    string[] pairs = new string[str.Length / 2];
+
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        pairs[i] = str.Substring((2 * i), 2);
+    }
+    return pairs;
+}
