7be755b43663b10b721e2885925c502fba2c1620
jnbeach <joseph.n.beach@outlook.com>
2021-12-21 15:49:06 -0600

    Updated MoleculesToAtoms
    Updated Bracket Finder function.

--- MoleculesToAtoms/Program.cs
+++ MoleculesToAtoms/Program.cs
@@ -18,34 +18,112 @@ namespace MoleculesToAtoms
                 Console.WriteLine($"{kvp.Key}: {kvp.Value}");
 
             }
-
+            List<List<int>> listOfBrackets = BracketFinder(inputMolecule);
+            foreach (List<int> BracketPair in listOfBrackets)
+            {
+                Console.WriteLine($"{BracketPair[0]} {BracketPair[1]} Mult:{BracketPair[2]}");
+            }
         }
         public static Dictionary<string, int> ParseMolecule(string formula)
         {
             Dictionary<string, int> dictOfAtoms = new Dictionary<string, int>();
-            List<int> indexList = new List<int>();
-            string upperFormula = formula.ToUpper();
-            string lowerFormula = formula.ToLower();
+
+            //First get all of the Atoms present in the string.
+            char ch;
+            char nextCh;
             for (int i = 0; i < formula.Length; i++)
             {
-                if (formula[i] == upperFormula[i])
+                ch = formula[i];
+                if (i != formula.Length - 1)
                 {
-                    indexList.Add(i);
+                    nextCh = formula[i + 1];
+                }
+                else
+                {
+                    nextCh = formula[i];
+                }
+                //Only do this if the character is Upper and is NOT the last character.
+                if (char.IsUpper(ch) && i != formula.Length - 1)
+                {
+                    //If the next character is lowercase, add the 2 letter atom.
+                    if (char.IsLower(nextCh))
+                    {
+                        if (!dictOfAtoms.ContainsKey(formula.Substring(i, 2)))
+                        {
+                            dictOfAtoms.Add(formula.Substring(i, 2), 1);
+                        }
+                    }
+                    //If the next character anything else, add the atom.
+                    else
+                    {
+                        if (!dictOfAtoms.ContainsKey(formula.Substring(i, 1)))
+                        {
+                            dictOfAtoms.Add(formula.Substring(i, 1), 1);
+                        }
+                    }
+                }
+                //If i is at the last character and the last character is Uppercase, then add the last character if it's not already contained.
+                else if (char.IsUpper(ch) && i == formula.Length - 1)
+                {
+                    if (!dictOfAtoms.ContainsKey(formula.Substring(i, 1)))
+                    {
+                        dictOfAtoms.Add(formula.Substring(i, 1), 1);
+                    }
                 }
             }
-            foreach (int index in indexList)
+            //Next count all of them
+            // ( = 40 ) = 41
+            // [ = 91 ] = 93
+            // { = 123 } = 124
+            // for (int i = 0; i < formula.length; i++)
+            // {
+
+            // }
+
+
+
+            // Do your science here :)
+            return dictOfAtoms;
+        }
+
+        public static List<List<int>> BracketFinder(string formula)
+        {
+            List<List<int>> listOfBrackets = new List<List<int>>();
+            List<int> bracketPair;
+            int bracketMult = 1;
+            for (int i = 0; i < formula.Length; i++)
             {
-                if (index + 1 >= formula.Length)
+                if (formula[i] == '(' || formula[i] == '[' || formula[i] == '{')
                 {
-                    dictOfAtoms.Add(formula[index], 1);
-                    break;
-                }
-                if (formula[index + 1])
-                {
+                    bracketPair = new List<int>();
+                    bracketPair.Add(i);
+                    if (formula[i] == '(') bracketPair.Add(formula.IndexOf(')', i));
+                    if (formula[i] == '[') bracketPair.Add(formula.IndexOf(']', i));
+                    if (formula[i] == '{') bracketPair.Add(formula.IndexOf('}', i));
+                    //BracketPair[1] is now the index of the closing bracket. If there is a number after the closing bracket, add the multiplying number to the bracket pair info.
+                    if (bracketPair[1] != formula.Length - 1)
+                    {
+                        if (char.IsDigit(formula[bracketPair[1] + 1]))
+                        {
+                            int digitCounter = 1;
+                            while (char.IsDigit(formula[bracketPair[1] + 1 + counter]))
+                            {
+                                digitCounter++;
+                            }
+                            bracketMult = Convert.ToInt32(formula.Substring(bracketPair[1] + 1, 1));
+                            bracketPair.Add(bracketMult);
+                        }
+                        else
+                        {
+                            bracketPair.Add(bracketMult);
+                        }
+                    }
+                    listOfBrackets.Add(bracketPair);
                 }
             }
 
-            // Do your science here :)
+            return listOfBrackets;
         }
+
     }
 }
