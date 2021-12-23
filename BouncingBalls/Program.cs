e26f21b5cd813c72d9d036656195d2818b2f7b09
jnbeach <joseph.n.beach@outlook.com>
2021-12-22 22:35:11 -0600

    BouncingBalls
    Updated solution for Bouncing Balls.

---
+++ BouncingBalls/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Bouncing Balls");
+double height = 3;
+double bounce = .66;
+double windowHeight = 1.5; // Result is -1
+Console.WriteLine($"{bouncingBall(height, bounce, windowHeight)}");
+
+static int bouncingBall(double h, double bounce, double window)
+{
+    if (h <= 0 || bounce < 0 || bounce >= 1 || window >= h) return -1;
+    int counter = 0;
+    while (h > window)
+    {
+        counter++;
+        h = bounce * h;
+        if (h > window) counter++;
+    }
+
+    return counter;
+}
